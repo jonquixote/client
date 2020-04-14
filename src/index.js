179cd59e237fc3b4affcf9dfe0b11fe64e06a479
“jonuixote” <jehawley@gmail.com>
2020-04-14 00:07:15 -0700

    hid keys & set PUBLIC_URL to index to get reseources

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createBrowserHistory } from 'history';
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
+
+export const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
